f976e0009c95b0a44b36864adf656e4b312ed28d
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-09-28 10:21:36 -0500

    Note Component

--- app/src/main/java/com/example/coopt2_fughetabout_it_inc/MainActivity.kt
+++ app/src/main/java/com/example/coopt2_fughetabout_it_inc/MainActivity.kt
@@ -4,20 +4,35 @@ package com.example.coopt2_fughetabout_it_inc
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.ui.Modifier
-import androidx.lifecycle.viewmodel.compose.viewModel
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContent {
-
+            NoteCard(Note("boo", "Jetpack Compose"))
         }
-
     }
+}
+
+data class Note(val title: String, val content: String)
+
+@Composable
+fun NoteCard(msg: Note) {
+    Column {
+        Text(text = msg.title)
+        Text(text = msg.content)
+    }
+}
+
+@Preview
+@Composable
+fun PreviewNoteCard() {
+    NoteCard(
+        msg = Note("Lexi", "Compose")
+    )
 }
