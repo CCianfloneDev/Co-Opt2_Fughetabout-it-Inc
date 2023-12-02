6a5da6f57887a8a0358dd0165f437306cef98050
Tadgh Henry <47073445+tadghh@users.noreply.github.com>
2023-12-02 15:57:54 -0600

    Update Room version 2.6.1, Kotlin to 1.9.10 and Kotlin compiler to 1.5.3 KSP now compiles with the latest version. Room doesnt like nullable values changes schema

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,10 +3,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
-
-
 }
-
 
 android {
     namespace = "com.example.coopt2_fughetabout_it_inc"
@@ -44,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
     packaging {
         resources {
@@ -54,28 +51,24 @@ android {
 }
 
 dependencies {
-   // implementation("com.google.devtools.ksp:symbol-processing:2.0.0-Beta1-1.0.15")
-    val room_version = "2.5.2"
+
+    val room_version = "2.6.1"
     implementation("com.google.devtools.ksp:symbol-processing-api:1.9.20-1.0.14")
 
     implementation("androidx.room:room-runtime:$room_version")
-    ksp("androidx.room:room-compiler:2.5.2")
+    ksp("androidx.room:room-compiler:2.6.1")
     implementation("androidx.room:room-ktx:$room_version")
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    //noinspection GradleCompatible
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
-    //testImplementation("junit:junit:4.13.2")
-    //androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    //androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    //androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     implementation("androidx.compose.ui:ui:1.5.4")
